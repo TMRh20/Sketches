9e9739bda824fe8cb2a5d0343b3883255cf334b7
TMRh20 <tmrh20@gmail.com>
2026-01-04 22:43:22 -0600

    Update client headless example
    - Add client timeouts application side
    - Ensure to clear incoming data upon disconnection
    - Renew address if connections fail

--- RF24Ethernet-lwIP/RF24Ethernet/examples/Headless/SimpleClient_Mesh_Headless/SimpleClient_Mesh_Headless.ino
+++ RF24Ethernet-lwIP/RF24Ethernet/examples/Headless/SimpleClient_Mesh_Headless/SimpleClient_Mesh_Headless.ino
@@ -1,17 +1,17 @@
 /*
  * *************************************************************************
- * RF24Ethernet Arduino library by TMRh20 - 2014-2015
+ * RF24Ethernet Arduino library by TMRh20 - 2014-2025
  *
  * Automated (mesh) wireless networking and TCP/IP communication stack for RF24 radio modules
  *
- * RF24 -> RF24Network -> UIP(TCP/IP) -> RF24Ethernet
+ * RF24 -> RF24Network -> uIP or lwIP(TCP/IP) -> RF24Ethernet
  *                     -> RF24Mesh
  *
  *      Documentation: http://nRF24.github.io/RF24Ethernet/
  *
  * *************************************************************************
  *
- * This example demonstrates how to configure a Headless Master node running an HTTP server
+ * This example demonstrates how to configure a Headless client connecting to an HTTP server
  * It does NOT require a Gateway node, just two nodes running RF24Ethernet
  *
  */
@@ -28,7 +28,7 @@
 #include "RF24Mesh.h"
 #include <RF24Ethernet.h>
 
-RF24 radio(7,8);
+RF24 radio(7, 8);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 RF24EthernetClass RF24Ethernet(radio, network, mesh);
@@ -43,18 +43,19 @@ RF52Network network(radio);
 RF52Mesh mesh(radio, network);
 RF52EthernetClass RF24Ethernet(radio, network, mesh);
 #endif
+
 EthernetClient client;
 
 // The hosts we will be connecting to
 // Note: The gateway will need to be able to forward traffic for internet hosts, see the documentation
-IPAddress ascii(10, 1, 3, 1);      //http://artscene.textfiles.com/asciiart/texthistory.txt
+IPAddress ascii(10, 1, 3, 1);  //http://artscene.textfiles.com/asciiart/texthistory.txt
 IPAddress host(ascii);
 
 void setup() {
 
   Serial.begin(115200);
   //printf_begin();
-  while(!Serial){}
+  while (!Serial) {}
   Serial.println(F("Start"));
 
   // Set the IP address we'll be using. The last octet mast match the nodeID (9)
@@ -65,14 +66,16 @@ void setup() {
   Ethernet.set_gateway(gwIP);
 
   Ethernet.begin(myIP);
+  //radio.begin();
+  //radio.setPALevel(RF24_PA_MIN,0);
   mesh.begin(65);
-
 
 }
 
 uint32_t counter = 0;
 uint32_t reqTimer = 0;
 uint32_t mesh_timer = 0;
+uint32_t clientTimeoutTimer = 0;
 
 void loop() {
 
@@ -92,7 +95,7 @@ void loop() {
 
   size_t size;
 
-  while ((size = client.available()) > 0) {
+  if ((size = client.available()) > 0) {
     char c = client.read();
     Serial.print(c);
     counter++;
@@ -100,6 +103,12 @@ void loop() {
 
   // if the server's disconnected, stop the client:
   if (!client.connected()) {
+
+    while ((size = client.available()) > 0) {
+      char c = client.read();
+      Serial.print(c);
+      counter++;
+    }
     Serial.println();
     Serial.println(F("Disconnect. Waiting for disconnect timeout"));
     client.stop();
@@ -113,6 +122,14 @@ void loop() {
     connect();
   }
 
+  // We need to implement a disconnection timer, in case we lose connection
+  // and the server stops responding
+  if(client.connected()){
+     if(millis() - clientTimeoutTimer > 30000){
+        client.stop();
+     }
+  }
+
   // We can do other things in the loop, but be aware that the loop will
   // briefly pause while IP data is being processed.
 }
@@ -122,6 +139,9 @@ void connect() {
 
   if (client.connect(host, 80)) {
     Serial.println(F("connected"));
+
+    // Reset the client connection timeout timer
+    clientTimeoutTimer = millis();
 
     // Make an HTTP request:
     if (host == ascii) {
@@ -134,5 +154,6 @@ void connect() {
   } else {
     // if you didn't get a connection to the server:
     Serial.println(F("connection failed"));
+    mesh.renewAddress();
   }
 }
